75affd0cc02338799ce6c24a39385ab05e1be16b
ammarlatif087 <93710117+ammarlatif087@users.noreply.github.com>
2023-02-13 19:33:31 +0500

    Create api_services.dart

---
+++ lib/api_Services/api_services.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+String apiKey = "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";
+
+class ApiServices {
+  static var baseUrl = Uri.parse("https://api.openai.com/v1/completions");
+
+  static Map<String, String> header = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer $apiKey'
+  };
+  static sendMessage(String? message) async {
+    print("api services");
+    print(message);
+    var res = await http.post(
+      baseUrl,
+      headers: header,
+      body: jsonEncode({
+        "model": "text-davinci-003",
+        "prompt": "$message",
+        "temperature": 0,
+        "max_tokens": 500
+      }),
+    );
+    if (res.statusCode == 200) {
+      var data = jsonDecode(res.body.toString());
+
+      var msg = data['choices'][0]['text'];
+
+      return msg;
+    } else {
+      print(res.statusCode);
+      print('Failed to fetch data');
+    }
+  }
+}
